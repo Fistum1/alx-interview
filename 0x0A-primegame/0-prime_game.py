3ddc9329d9fdd1f5469bbb437e871d2b2efb943c
fistum1 <fistumitiku1@gmail.com>
2024-03-12 22:59:35 +0300

    0x0A-primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,19 +1,65 @@
 #!/usr/bin/python3
-""" A prime game interview question """
+"""
+    Module defining isWinner function.
+"""
 
 
-def get_first_prime(values):
-    for i in values:
-        if isPrime(i):
-            return [v for v in values if v % i != 0]
-    return False
+def isWinner(x, nums):
+    """
+	Function to get who has won in prime game
+    """
+    mariaWinsCount = 0
+    benWinsCount = 0
+
+    for num in nums:
+        roundsSet = list(range(1, num + 1))
+        primesSet = primes_in_range(1, num)
+
+        if not primesSet:
+            benWinsCount += 1
+            continue
+
+        isMariaTurns = True
+
+        while(True):
+            if not primesSet:
+                if isMariaTurns:
+                    benWinsCount += 1
+                else:
+                    mariaWinsCount += 1
+                break
+
+            smallestPrime = primesSet.pop(0)
+            roundsSet.remove(smallestPrime)
+
+            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
+
+            isMariaTurns = not isMariaTurns
+
+    if mariaWinsCount > benWinsCount:
+        return "Winner: Maria"
+
+    if mariaWinsCount < benWinsCount:
+        return "Winner: Ben"
+
+    return None
 
 
-def isPrime(x):
-    """ It checks if number is a prime number """
-    if x < 2 or x == 4:
+def is_prime(n):
+    """
+	Returns True if n is prime, else False.
+    """
+    if n < 2:
         return False
-    for i in range(2, x // 2):
-        if x % i == 0:
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
             return False
     return True
+
+
+def primes_in_range(start, end):
+    """
+	Returns a list of prime numbers between start and end (inclusive).
+    """
+    primes = [n for n in range(start, end+1) if is_prime(n)]
+    return primes
